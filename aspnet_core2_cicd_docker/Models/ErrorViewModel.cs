ebbfca10768e6caad8de6bf6beb055606f3203e0
Manoj Kumar <erpatel@gmail.com>
2017-11-21 15:31:21 -0800

    Add project files.

---
+++ aspnet_core2_cicd_docker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnet_core2_cicd_docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
